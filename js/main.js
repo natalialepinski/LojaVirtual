ecf5a5632a8b6cf677f59ddea1da35e1baa0d37d
Natalia Lepinski <nlepinski@dte.local>
2018-04-03 19:02:44 -0300

    main modificada, partials criadas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-angular.module('lojaApp', ['myDirectives']);
+angular.module('LojaVirtual', [''])
+	.config(['$routeProvider', function(){
+		$routeProvider.
+			when('/home', {
+				templateUrl: 'partials/home.html',
+				controller: 'storeController'
+			}).
+			when('/produtos', {
+				templateUrl: 'partials/produtos.html',
+				controller: 'storeController'
+			}).
+			when('/carrinho', {
+				templateUrl: 'partials/carrinho.html',
+				controller: 'storeController'
+			}).
+			otherwise({
+				redirectTo: '/home'
+			});
+	}])
+	.factory("DataService", function(){
+		
+		// create a store
+		var myHome = new home();
+
+		// create a shopping cart
+		var myCart = new carrinho("LojaVirtual");
+
+		return {
+			home: myHome,
+			carrinho: myCart,
+		};
+	});
+
