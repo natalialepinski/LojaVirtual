ec78acab75a54a068dead518cf130a8026130434
Natalia Lepinski <nlepinski@Natalias-MacBook-Air.local>
2018-05-01 17:48:25 -0300

    criada controllers e service

--- js/main.js
+++ js/main.js
@@ -1,28 +1,56 @@
-angular.module('storeApp', [''])
-	.config(['$routeProvider', function(){
+angular.module('storeApp', ['ngRoute'])
+	.service('productService', [function() {
+		this.getProducts = function() {
+			return [{
+				id: 1,
+				name: 'Mochila Magenta',
+				description: 'xxxxxxxxxxxx',
+				price: 20,
+				photos: [
+					'img/mochila_magenta.png'
+				],
+				quantity: 10
+			}, {
+				id: 2,
+				name: 'Mochila Magenta',
+				description: 'xxxxxxxxxxxx',
+				price: 20,
+				photos: [
+					'img/mochila_magenta.png'
+				],
+				quantity: 0
+			}];
+		}
+	}])
+	.controller('storeController', ['$scope', 'productService', function($scope, productService) {
+		var products = $scope.products = productService.getProducts();
+	}])
+	.controller('productController', ['$scope', 'productService', '$routeParams', function($scope, productService, $routeParams) {
+		var product = $scope.product = productService.getProducts().find(function(product){
+			return product.id === parseInt($routeParams.id);
+		}); 
+	}])
+	.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
 		$routeProvider.
 			when('/home', {
 				templateUrl: 'partials/home.html',
-				controller: 'storeController',
-				controllerAs: ''
+				controller: 'storeController'
 			}).
 			when('/products', {
 				templateUrl: 'partials/products.html',
 				controller: 'productsController'
-				controllerAs: ''
 			}).
-			when('/product', {
+			when('/product/:id', {
 				templateUrl: 'partials/product.html',
-				controller: 'productController',
-				controllerAs: ''
+				controller: 'productController'
 			}).
 			when('/cart', {
 				templateUrl: 'partials/cart.html',
 				controller: 'cartController'
-				controllerAs: ''
 			}).
 			otherwise({
 				redirectTo: '/home'
 			});
+		$locationProvider.html5Mode(true);
 	}]);
 
