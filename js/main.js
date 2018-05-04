3bc70f4279f1c7dd38051c2e822f1fcc4c915913
Natalia Lepinski <nlepinski@Natalias-MacBook-Air.local>
2018-05-04 16:02:57 -0300

    Add cart & product pages

--- js/main.js
+++ js/main.js
@@ -3,22 +3,54 @@ angular.module('storeApp', ['ngRoute'])
 		this.getProducts = function() {
 			return [{
 				id: 1,
-				name: 'Mochila Magenta',
-				description: 'xxxxxxxxxxxx',
-				price: 20,
+				name: 'Mochila Rosa',
+				description: '100% Poliéster',
+				fulldescription: 'Produzido em Poliéster. Possui alças e costas espumadas que proporciona maior conforto para os ombros, bolso frontal com fechamento por zíper, compartimento para guardar seu laptop.',
+				price: 50,
 				photos: [
 					'img/mochila_magenta.png'
 				],
 				quantity: 10
-			}, {
+			},{
 				id: 2,
-				name: 'Mochila Magenta',
-				description: 'xxxxxxxxxxxx',
-				price: 20,
+				name: 'Mochila Azul',
+				description: '100% Poliéster',
+				fulldescription: 'Produzido em Poliéster. Possui alças e costas espumadas que proporciona maior conforto para os ombros, bolso frontal com fechamento por zíper, compartimento para guardar seu laptop.',
+				price: 50,
 				photos: [
-					'img/mochila_magenta.png'
+					'img/mochila_azul.png'
 				],
 				quantity: 0
+			},{
+				id: 3,
+				name: 'Mochila Branca',
+				description: '100% Poliéster',
+				fulldescription: 'Produzido em Poliéster. Possui alças e costas espumadas que proporciona maior conforto para os ombros, bolso frontal com fechamento por zíper, compartimento para guardar seu laptop.',
+				price: 50,
+				photos: [
+					'img/mochila_branca.png'
+				],
+				quantity: 20
+			},{
+				id: 4,
+				name: 'Carteira Feminina',
+				description: 'Porta Celular',
+				fulldescription: 'Produzida em couro sintético. Possui bolso interno com zíper e divisora para cartões de credito, com alça de mão, porta celular interno e seu fechamento por botão.',
+				price: 25,
+				photos: [
+					'img/carteira_preta.png'
+				],
+				quantity: 50
+			},{
+				id: 5,
+				name: 'Carteira Masculina',
+				description: 'Produzida em couro',
+				fulldescription: 'Produzida em couro. Possui porta níquel, porta notas e porta documentos.',
+				price: 45,
+				photos: [
+					'img/carteira_couro.png'
+				],
+				quantity: 15
 			}];
 		}
 	}])
